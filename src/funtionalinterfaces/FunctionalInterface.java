d5c58f128bed1d966ec83dd2a2a1f92ff10d709a
Vishnu_Praveen <vishnupraveenblr@gmail.com>
2020-05-23 09:14:07 +0530

    Functional Interfaces

---
+++ src/funtionalinterfaces/FunctionalInterface.java
@@ -0,0 +1,38 @@
+package funtionalinterfaces;
+
+import java.time.LocalTime;
+import java.util.Objects;
+
+public class FunctionalInterface {
+
+    public static void main(String[] args) {
+        System.out.println(composeHashCode("Hello", "World"));
+        System.out.println(composeHashCode("Hello", null));
+    }
+
+  /*  public static int composeHashCode(Object a, Object b)
+    {
+        return  a.hashCode()^b.hashCode();
+    }*/
+
+    public static String getApplicationStatus()
+    {
+        System.out.println("getApplicationStatus");
+        return "It's "+ LocalTime.now();
+    }
+
+    //Here the funtion getApplicationStatus is called 4 times even if its null
+ /* public static int composeHashCode(Object a, Object b)
+  {//identify which argument is null
+      Objects.requireNonNull(a,"a may not be null " + getApplicationStatus());
+      Objects.requireNonNull(b,"a may not be null " + getApplicationStatus());
+      return  a.hashCode()^b.hashCode();
+  }
+  */
+    public static int composeHashCode(Object a, Object b)
+    {
+        Objects.requireNonNull(a,()->"a may not be null " + getApplicationStatus());//supplier of string here only invoked for null arguments
+        Objects.requireNonNull(b,()->"b may not be null " + getApplicationStatus());
+        return  a.hashCode()^b.hashCode();
+    }
+}
